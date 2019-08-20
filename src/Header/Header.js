f502ea99f284065f4dd953a682f0b9e263a619d4
Shawn Pereira <recurshawn@users.noreply.github.com>
2019-08-20 23:25:07 +0530

    Centered logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,8 +4,10 @@ import './Header.css';
 const Header = () => {
     return(
     <div className="Header">
+        <div className="logotext">
         <img class="logo" src={Logo} alt="WhatsAppr Logo"/>
         <h1>WhatsAppr</h1>
+        </div>
     </div>
     );
 }
