13bf4ca86692c0571be955738889ee98b8b173c3
Shawn Pereira <40794711+recurshawn@users.noreply.github.com>
2021-05-16 23:54:22 +0530

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <p>Made with <Emoji symbol="❤️" label="love"/> by <a href="https://recurshawn.github.io/">Shawn Pereira</a> Twitter:<a href="https://twitter.com/Incept_shawn">@Incept_shawn</a>
         <br/><a target="_blank" rel="noopener noreferrer" href="https://ko-fi.com/recurshawn"><Emoji symbol="🚀" label="rocket"/> Support this project!</a></p>
         Disclaimer: WhatSharer is not affiliated with WhatsApp Inc.
-        <br/>2020 © WhatSharer | All rights reserved
+        <br/>2021 © WhatSharer | All rights reserved
     </div>
     );
 }
